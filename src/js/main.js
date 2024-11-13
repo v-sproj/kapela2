e79c531f5b9e776b1deda773cbe26ea5cf9e9cd4
v-sproj <sprojcarova.val@seznam.cz>
2024-11-13 19:13:07 +0100

    další večerní intr commit

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,12 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import {Dropdown} from 'bootstrap';
 
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+Fancybox.bind('[data-fancybox]', {
+  }); 
+
+  
+
+
