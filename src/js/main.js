b10b9c4ca648fcbb94f69789bed1af3d1aef58cd
v-sproj <sprojcarova.val@seznam.cz>
2024-11-19 15:34:39 +0100

    hotovo

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,14 @@
 import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
-import {Dropdown} from 'bootstrap';
+import { Dropdown } from 'bootstrap';
 
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 Fancybox.bind('[data-fancybox]', {
-  }); 
+});
 
-  
+
 
 
